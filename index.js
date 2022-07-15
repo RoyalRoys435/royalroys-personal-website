4c324d1218c02a09ed9d15b194b5a2c637426946
Douglas muraya <80788182+RoyalRoys435@users.noreply.github.com>
2022-07-15 14:53:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//typing animation
+
+var typed = new Typed(".typing", {
+    strings: [ " " ,"Cloud Engineer.", " Game Designer and Developer.", "android developer.", "Software Developer.","Cloud Engineer."],
+    typeSpeed: 110,
+    BackSpeed: 100,
+   loop:true
+})
+
